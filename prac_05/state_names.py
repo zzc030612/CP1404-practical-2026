f976a58ca7c390148d36018914af888ac6efa349
zzc030612 <2683332593@qq.com>
2026-03-02 19:19:07 +0800

    reformat state names program per PEP 8

---
+++ prac_05/state_names.py
@@ -0,0 +1,19 @@
+"""
+CP1404/CP5632 Practical
+State names in a dictionary
+File needs reformatting
+"""
+
+# TODO: Reformat this file so the dictionary code follows PEP 8 convention
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:3} is {name}")
+
+state_code = input("\nEnter short state: ").upper()
+while state_code:
+    try:
+        print(f"{state_code} is {CODE_TO_NAME[state_code]}")
+    except KeyError:
+        print("Invalid short state")
+    state_code = input("Enter short state: ").upper()
